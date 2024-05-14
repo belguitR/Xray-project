3e0af4403c86c821ede8075efc02a5b18953b9bb
AmirNasfy <amirnasfy01@gmail.com>
2024-05-14 10:31:01 +0100

    image-scale/box-color

--- DentalXrayAI-main/app.py
+++ DentalXrayAI-main/app.py
@@ -58,9 +58,10 @@ def detect_objects_on_image(buf):
         prob = round(box.conf[0].item(), 2)
         label = result.names[class_id]
         
-        cv2.rectangle(img_cv2, (x1, y1), (x2, y2), (255,0,0), 2)
+        cv2.rectangle(img_cv2, (x1, y1), (x2, y2), (0,255,255), 2)
         
-        cv2.putText(img_cv2, f'{label}: {prob}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
+        cv2.putText(img_cv2, f'{label}: {prob}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255), 2)
+    
     
     # Convert the annotated image back to bytes
     _, img_encoded = cv2.imencode('.jpg', img_cv2)
